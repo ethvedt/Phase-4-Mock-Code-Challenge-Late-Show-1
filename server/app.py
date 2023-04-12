972d949824dcfcf92e382090064451cfdeb9fa7b
Eric Thvedt <ericsthvedt@gmail.com>
2023-04-12 11:33:20 -0700

    time limit reached, all deliverables but POST working

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import Flask, request, make_response
 from flask_migrate import Migrate
 
 from models import db, Episode, Guest, Appearance
@@ -17,6 +17,61 @@ db.init_app(app)
 @app.route('/')
 def home():
     return ''
+
+@app.route('/episodes', methods=['GET'])
+def episodes():
+    episode_list = []
+    for episode in Episode.query.all():
+        ep_dict = episode.to_dict(only=('id', 'date', 'number'))
+        episode_list.append(ep_dict)
+    return make_response(episode_list, 200)
+    
+@app.route('/episodes/<int:id>', methods=['GET','DELETE'])
+def episode_by_id(id):
+    episode = Episode.query.filter(Episode.id == id).first()
+    if not episode:
+        return make_response(
+            {"error": "404: Episode not found"},
+            404
+        )
+    if request.method == 'GET':
+        return make_response(episode.to_dict(), 200)
+        
+    elif request.method == 'DELETE':
+        db.session.delete(episode)
+        db.session.commit()
+        return make_response('',204)
+        
+@app.route('/guests', methods=['GET'])
+def guests():
+    guest_list = []
+    guests = Guest.query.all()
+    for guest in guests:
+        guest_dict = guest.to_dict(only=('id', 'name', 'occupation'))
+        guest_list.append(guest_dict)
+    return make_response(guest_list, 200)
+
+@app.route('/appearances', methods=['POST'])
+def new_app():
+    if request.method == 'POST':
+        try:
+            appearance = Appearance(
+                rating=request.form.get('rating'),
+                episode_id=request.form.get('episode_id'),
+                guest_id=request.form.get('guest_id')
+            )
+            db.session.add(appearance)
+            db.session.commit()
+            return make_response(
+                appearance.to_dict(),
+                201
+            )
+        except ValueError:
+            return make_response(
+                {"error": "400: Validation error."},
+                400
+            )
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -14,18 +14,47 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 class Episode(db.Model, SerializerMixin):
-    __tablename__ = 'episodes'
+    __tablename__ = 'episode'
 
     id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String)
+    number = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+
+    appearance = db.relationship('Appearance', backref='episode', cascade='all, delete-orphan')
+    guest = association_proxy('guests', 'appearance')
+
+    serialize_rules = ('-created_at', '-updated_at')
 
 class Guest(db.Model, SerializerMixin):
-    __tablename__ = 'guests'
-
+    __tablename__ = 'guest'
+    
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    occupation = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
 
+    appearance = db.relationship('Appearance', backref='guest')
+    episode = association_proxy('episodes', 'appearance')
+    
+    serialize_rules = ('-created_at', '-updated_at')
+    
 class Appearance(db.Model, SerializerMixin):
-    __tablename__ = 'appearances'
+    __tablename__ = 'appearance'
 
     id = db.Column(db.Integer, primary_key=True)
+    episode_id = db.Column(db.Integer, db.ForeignKey('episode.id'))
+    guest_id = db.Column(db.Integer, db.ForeignKey('guest.id'))
+    rating = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
 
-# add any models you may need. 
+    serialize_rules = ('-created_at', '-updated_at')
+
+    @validates('rating')
+    def validate_rating(self, key, value):
+        if value not in [1, 2, 3, 4, 5]:
+            raise ValueError('Rating must be between 1 and 5.')
+        return value
